0ce2aa52e4dee64052a8ca4a62f3121aa018a553
NeilChavez <lolochavez22@icloud.com>
2023-01-08 19:05:14 -0500

    Fix color picker

--- App/components/ColoPicker.js
+++ App/components/ColoPicker.js
@@ -1,21 +1,21 @@
 export default function ColorPicker() {
 
-  document.addEventListener("click", (e) => {
-    if (e.target.matches(".color-blue")) {
-        document.body.style.setProperty('background-color', "var(--color-blue)")
-    }
-    if (e.target.matches(".color-red")) {
-        document.body.style.setProperty('background-color', "var(--color-orange)")
-    }
-    if (e.target.matches(".color-yellow")) {
-        document.body.style.setProperty('background-color', "var(--color-yellow)")
-    }
-  });
+    document.addEventListener("click", (e) => {
+        if (e.target.matches(".color-blue")) {
+            document.body.style.setProperty('background-color', "var(--color-blue-sea)")
+        }
+        if (e.target.matches(".color-violet")) {
+            document.body.style.setProperty('background-color', "var(--color-violet)")
+        }
+        if (e.target.matches(".color-yellow")) {
+            document.body.style.setProperty('background-color', "var(--color-yellow)")
+        }
+    });
 
-  return `
+    return `
         <div class="picker-color">
             <div class="color-blue"></div>
-            <div class="color-red"></div>
+            <div class="color-violet"></div>
             <div class="color-yellow"></div>
         </div>
     `;
--- App/components/Modal.js
+++ App/components/Modal.js
@@ -1,20 +1,20 @@
 export function Modal(counter, totalQuestions) {
-const handlerClick = (e) => {
-  
-  if (e.target.matches(".btn-close") || e.target.matches(".modal:not(.modal-article)")){
-    const $modal = document.querySelector(".modal");
-      if(!$modal) return false;
-        $modal.open = false;
-        $modal.remove();
-        return document.removeEventListener('click', handlerClick);
-  }
-} 
+  const handlerClick = (e) => {
 
+    if (e.target.matches(".btn-close") || e.target.matches(".modal:not(.modal-article)")) {
+      const $modal = document.querySelector(".modal");
+      if (!$modal) return false;
+      $modal.open = false;
+      $modal.remove();
+      return document.removeEventListener('click', handlerClick);
+    }
+  }
+  const color = window.getComputedStyle(document.querySelector("body")).backgroundColor
   document.addEventListener("click", handlerClick);
 
   return `
     <dialog class="modal" open>     
-        <article class="modal-article">
+        <article class="modal-article" style="background-color: ${color}">
             <h2>You answered ${counter} out of ${totalQuestions} questions correctly!</h2>
             <i class="fa-solid fa-x btn-close"></i>
         </article>
